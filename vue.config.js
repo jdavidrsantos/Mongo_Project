0fdd233deb9a2abfeb223ac7f747e41a1bb8462c
JDRS <jdavidrsantos@gmail.com>
2023-11-17 19:54:20 -0500

    deploy GitPages
    deploy GitPages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/dashboard2/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Mongo_Project/' : '/',
 })
